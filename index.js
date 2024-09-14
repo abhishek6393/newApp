09e992845fd3fd6e92ca02d3a6349a19af7e8ef0
abhishek6393 <abhishekns063@gmail.com>
2024-09-14 14:31:23 +0530

    neew one

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("it ia starting point");
+const express=require("express");
+require('dotenv').config()
+
+
+let app=express();
+
+
+let port=8080;
+
+
+app.listen(process.env.PORT,()=>{
+
+
+    console.log(`The request is running at the port number ${port}`);
+    
+})
+
+
+
+app.get("/",(req,res)=>{
+
+
+    res.send("It is a home request");
+})
+
+app.get("/abhi",(req,res)=>{
+
+
+    res.send("It is a user request");
+})
+app.get("/login",(req,res)=>{
+
+
+    res.send("It is a login request");
+})
+
+
+
